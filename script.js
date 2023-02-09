2213647b31bbf94505e9b3659dd12a2fb142d0e8
jcyepesgarcia <jcyepesgarcia@gmail.com>
2023-02-09 14:32:10 -0500

    Función eliminar tarea agregada

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-(() => {
-  const btn = document.querySelector("[data-form-btn]");
+import { checkComplete } from "./components/checkComplete.js";
+import { deleteIcon } from "./components/deleteIcon.js";
+
+const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (e) => {
   e.preventDefault();
@@ -14,34 +16,10 @@ const createTask = (e) => {
   titleTask.innerHTML = value;
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
-  taskContent.appendChild(deleteTask());
-  const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
   task.appendChild(taskContent);
   list.appendChild(task);
-  input.value = '';
+  task.appendChild(deleteIcon());
+  input.value = "";
 };
 
 btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-  const i = document.createElement('i');
-  i.classList.add('far', 'fa-check-square', 'icon');
-  i.addEventListener("click", completeTask);
-  return i;
-}
-
-const deleteTask = () => {
-  const i = document.createElement('i');
-  i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
-  i.addEventListener("click", completeTask);
-  return i;
-}
-
-
-const completeTask = (e) => {
-  const element = e.target;
-  element.classList.toggle('far');
-  element.classList.toggle('fas');
-  element.classList.toggle('completeIcon');
-}
-})()
